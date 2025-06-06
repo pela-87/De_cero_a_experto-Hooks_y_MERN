53ec9459e5947593b702abc862ef952f1adb8d85
Jonatan <villarrueljonatan@gmail.com>
2025-06-06 20:02:32 -0300

    Comenzando el proyecto con React y Vite.

---
+++ 03-counter-app-vite/src/HelloWorldApp.jsx
@@ -0,0 +1,11 @@
+// function App() {
+//     return <h1>Hola Mundo..!!</h1>;
+// }
+
+export const HelloWorldApp = () => {
+  return (
+    <h1>Hello World App</h1>
+  )
+};
+
+//export const HelloWorldApp = () => <h1>Arrow function</h1>;
