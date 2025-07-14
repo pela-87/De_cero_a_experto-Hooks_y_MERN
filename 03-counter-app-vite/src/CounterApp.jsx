163668f816f7b7c6eef1b4e27daa41e6b35a5651
Jonatan <villarrueljonatan@gmail.com>
2025-07-14 20:44:28 -0300

    Termino el modulo de componentes y comienza las pruebas unitarias

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,24 +1,44 @@
+import { useState } from 'react';
 //import PropTypes from 'prop-types';
 
 export const CounterApp = ( { value } ) => {
     
     // function heandleAdd (event) {
-    //     console.log(event)
-    // }
+        //     console.log(event)
+        // }
+
+    //Hooks
+    const [counter, setCounter] = useState(value);
 
     /* Cuando una funcion no tiene mucho que ver con el componente es aconsejable sacarlo a fuera del componente. */
     const heandleAdd = () => { 
         //console.log( event ) 
-        value = 1000;
-        console.log(value);
+        // value = 1000;
+        // console.log(value);
+        
+        //Hooks
+        //setCounter(counter + 1);
+        //setCounter( () => counter + 1);
+        setCounter( ( c ) => c + 1);
+
     }
+
+    const heandleResta = () => setCounter( counter - 1);
+
+    const heandleReset = () => setCounter(value);
     
     return (
         <>
             <h1>CounterApp</h1>
-            <h2> { value } </h2>
-            <button onClick={ heandleAdd }>
-                +1
+            <h2> { counter } </h2>
+            <button onClick={ heandleAdd }> 
+                +1 
+            </button>
+            <button onClick={ heandleResta }> 
+                -1 
+            </button>
+            <button onClick={ heandleReset }> 
+                Reset 
             </button>
         </>
     )
---
+++ 03-counter-app-vite/tests/demo.test.js
@@ -0,0 +1,5 @@
+test('Esto es el titulo del test', () => {
+    if (1 === 0) {
+        throw Error ('No se puede dividir por 0.');
+    };
+});
