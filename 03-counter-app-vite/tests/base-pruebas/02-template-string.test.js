156a078a60001644f0e551d2d2d35107b242511a
Jonatan <villarrueljonatan@gmail.com>
2025-07-17 19:51:54 -0300

    Clase 57 pruebas unitarias. No funcionan por un problema en yarn.

---
+++ 03-counter-app-vite/tests/base-pruebas/02-template-string.test.js
@@ -0,0 +1,10 @@
+import { getSaludo } from '../../src/base-pruebas/02-template-string';
+
+describe ('Probando el 02-template-string', () => {
+    // test('el getSaludo debe devolver "Hola, y el nombre"', () => {
+    //     const name = 'Pela';
+    //     const message = getSaludo( name );
+
+    //     expect( message ).toBe(`Hola ${ name }`);
+    // });
+});
