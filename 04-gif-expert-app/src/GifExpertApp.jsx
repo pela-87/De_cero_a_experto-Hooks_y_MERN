d003c6bf4f77d637636d066d44ba515be8e8116a
Jonatan <villarrueljonatan@gmail.com>
2025-08-06 20:05:52 -0300

    Clase 79 proyecto de gif

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,8 +1,17 @@
 import { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
 
 export const GifExpertApp = () => {
     
     const [ categories, setCategories ] = useState(['One Punch', 'Dragon Ball']);
+
+    const onAddCategory = () => {
+        //const newCategory = 'Samurai X';
+        //setCategories([ ...categories, newCategory ]);
+        setCategories([ 'Samurai X', ...categories ]);
+        //setCategories( cats => [ ...cats, newCategory ] );
+        
+    }
 
   return (
     <>
@@ -10,6 +19,9 @@ export const GifExpertApp = () => {
         <h1>GifExpertApp</h1>
 
         {/* Input */}
+        < AddCategory />
+
+        <button onClick={ onAddCategory }>Agregar</button>
     <ol>
         { categories.map( (category) => {
             return <li key={ category }>
